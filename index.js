0533a20042efde2724f37d1fd7f405687a40c61a
Jesse Greenwald <jesse@swsb.io>
2016-07-07 15:26:45 -0500

    Added margin support

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var qrcode = require('./lib/qrcode.js');
 module.exports = {
     typeNumber: 4,
     errorCorrectLevel: 'L',
-    toBase64: function(text, size){
+    toBase64: function(text, size, margin){
         var qr = qrcode(this.typeNumber, this.errorCorrectLevel);
         qr.addData(text);
         qr.make();
-        var base64 = qr.createImgBase64(size);
+        var base64 = qr.createImgBase64(size, margin);
         return base64;
     },
-    toDataURL: function(text, size){
-        var base64 = this.toBase64(text, size);        
+    toDataURL: function(text, size, margin){
+        var base64 = this.toBase64(text, size, margin);        
         var dataURL = 'data:image/gif;base64,' + base64; 
         return dataURL;
     }
